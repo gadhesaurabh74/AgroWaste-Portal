3e71da3571fb12af4383efc3e18245681dbf90a5
Saurabh Gadhe <gadhesaurabh74@gmail.com>
2025-01-18 15:56:22 +0530

    file structure for frontend and backend is completed

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,26 @@
 const express=require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+const expressSession=require('express-session');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.EXPRESS_SESSION_SECRET
+}))
+
+
+app.use("/",indexRouter);
+app.use("/owners",ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productsRouter);
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
 
